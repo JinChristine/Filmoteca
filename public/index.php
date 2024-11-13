5ea1788b8aaef2d6483f4ded4d6b4915e4260867
JinChristine <jin.christine_0111@hotmail.com>
2024-11-13 15:24:27 +0000

    update avec app + namespace

--- public/index.php
+++ public/index.php
@@ -1,31 +1,8 @@
-<!DOCTYPE html>
-<html>
 <?php
-/* //Méthode 1: en PHP
-// $_SERVER['REQUEST_URI']: le chemin de l'URL
-$uri = $_SERVER['REQUEST_URI'];
 
-echo var_dump($uri);
-$pagesAutorisees = ['Accueil-Filmoteca', 'CRUD', 'ListeFilm'];
+require __DIR__ . '/../vendor/autoload.php';
 
-if (in_array($uri, $pagesAutorisees)) {
-    include "$uri.php";
-} else {
-    include '../Accueil-Filmoteca.php';
-}
-*/
+use App\Core\Router;
 
-//Méthode 2: en POO
-require("./../Routeur.php");
-$objet = new Routeur();
-$objetView = $objet->route();
-$pagesAutorisees = ['Accueil-Filmoteca', 'CRUD', 'ListeFilm'];
-if (in_array($ojetView, $pagesAutorisees)) {
-    include "./../$objetView.php";
-} else {
-    include '../Accueil-Filmoteca.php';
-}
-echo $objetView ;
-?>
-
-</html>
+$router = new Router();
+$router->route();
