ffec95a6485daec95db19969827631e605f90093
JinChristine <jin.christine_0111@hotmail.com>
2024-11-13 12:37:36 +0100

    update index.php avec liste de pages autorisées d'accès

--- public/index.php
+++ public/index.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html>
-    <?php
-    // $_SERVER['REQUEST_URI']: le chemin de l'URL
-    $uri = $_SERVER['REQUEST_URI'];
-    $page = trim($uri, '/');
+<?php
+// $_SERVER['REQUEST_URI']: le chemin de l'URL
+$uri = $_SERVER['REQUEST_URI'];
 
-    echo var_dump($page);
-    if (isset($page)) {
-        if ($page == "Liste%20de%20films") {
-            include "../ListeFilm.php";
-        } 
-        include "../$page.php";
-    } 
-    else {
-        include "../Accueil-Filmoteca.php";
-    }
-       
-    ?>
+echo var_dump($uri);
+$pagesAutorisees = ['Accueil-Filmoteca', 'CRUD', 'ListeFilm'];
+
+if (in_array($uri, $pagesAutorisees)) {
+    include "$uri.php";
+} else {
+    include '../Accueil-Filmoteca.php';
+}
+
+?>
+
 </html>
