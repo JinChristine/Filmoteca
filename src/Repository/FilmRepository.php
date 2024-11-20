94c66586afa541a7e3e5782231b9e8db7acd4d80
JinChristine <jin.christine_0111@hotmail.com>
2024-11-20 13:04:25 +0000

    update FilmRepository

--- src/Repository/FilmRepository.php
+++ src/Repository/FilmRepository.php
@@ -4,16 +4,25 @@ use App\Core\DatabaseConnection;
 
 public class FilmRepository
 {
+    private \PDO $db;
+
     public function __contruct (){
         $this->db = DatabaseConnection::getConnection();
     }
 
+    public function findAll(): array
+    {
+        $query = 'SELECT * FROM film';
+        $stmt = $this->db->query($query);
+
+        return $stmt->fetchAll();
+    }
+/*
     // Ajouter un film
     public function addFilm(Film $film)
-    {
-        $sql = query("INSERT INTO film() VALUES ()");
-        $sth = $db->exec($sql);
+    {  
     }
+
     // Lire un film
     public function getFilm(int $id)
     {
@@ -22,30 +31,6 @@ public class FilmRepository
     // Mettre à jour un film
     public function updateFilm(int $id, string $title, string $year, string $genre, string $synopsis, string $director, string $created_at)
     {
-        
-        $sql = 'UPDATE film SET title=$title, year=$year, type=$type, synopsis=$id, director=$director, created_at=$created_at, updated_at=$date';
-
-        foreach($dbh->query($sql) as $row)
-        {
-            if ($row['id'] == $id){
-                echo $row['id']. ' '.$row['title']. ' '. $row['year']. ' by '. $row['director'];
-            }
-        }
-        return null;
-
-
-        foreach($this->films as $film)
-        {
-            if ($film.getId() == $id){
-                $film->setTitle($title);
-                $film->setDirector($director);
-                $film->setYear($year);
-                $film->setGenre($genre);
-                $film->setSynopsis($synopsis);
-                $film->setCreated($created_at);
-            }
-        }
-        return null;
     }
     // Lire tous les films
     public function getAllFilms()
@@ -54,16 +39,9 @@ public class FilmRepository
     }
     // Supprimer un film
     public function deleteFilm(int $id)
-    {   
-        foreach($this->films as $film){
-            if(!$film.getId() == $id){
-                echo "Id invalide";
-            return;
-            }
-        }
-        $this->films = array_filter($this->films, function (Film $film) use ($id)); // array_filter cache un film de la liste de film
+    {
     }
-
+*/
     
 }
 ?>
