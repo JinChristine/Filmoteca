bd6830e06535cbaa30b649164a88974d0a17bbe0
JinChristine <jin.christine_0111@hotmail.com>
2024-11-20 11:33:17 +0000

    update film.php

--- src/Entity/Film.php
+++ src/Entity/Film.php
@@ -7,12 +7,12 @@ class Film
     private int $id;
     private string $title;
     private string $director;
-    private string $year;
-    private string $created_at;
+    private int $year;
+    private \DateTime $created_at;
     private string $genre;
     private string $synopsis;
-    private string $deleted_at;
-    private string $updated_at;
+    private \DateTime $deleted_at;
+    private \DateTime $updated_at;
 
     public function __construct(int $id, string $title, string $director, string $genre, string $year, string $synopsis, string $created_at, string $deleted_at, string $updated_at)
     {
@@ -29,86 +29,94 @@ class Film
     }
 
     // Getters
-    public function getId()
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function getTitle()
+    public function getTitle(): string
     {
         return $this->title;
     }
 
-    public function getDirector()
+    public function getDirector(): string
     {
         return $this->director;
     }
 
-    public function getYear()
+    public function getYear(): int
     {
         return $this->year;
     }
 
-    public function getSynopsis()
+    public function getSynopsis(): string
     {
         return $this->synopsis;
     }
 
-    public function getGenre()
+    public function getGenre(): string
     {
         return $this->genre;
     }
-    public function getCreated()
+    public function getCreated(): \DateTime
     {
         return $this->created_at;
     }
 
-    public function getDeleted()
+    public function getDeleted(): \DateTime
     {
         return $this->deleted_at;
     }
-    public function getUpdated()
+    public function getUpdated(): \DateTime
     {
         return $this->updated_at;
     }
 
     // Setters
-    public function setTitle(string $title)
+    public function setTitle(string $title): self
     {
         $this->title = $title;
+        return $this;
     }
 
-    public function setDirector(string $director)
+    public function setDirector(string $director): self
     {
         $this->director = $director;
+        return $this;
     }
-    public function setYear(string $year)
+    public function setYear(string $year): self
     {
         $this->year = $year;
+        return $this;
     }
 
-    public function setGenre(string $genre)
+    public function setGenre(string $genre): self
     {
         $this->genre = $genre;
+        return $this;
     }
     
-    public function setSynopsis(string $synopsis)
+    public function setSynopsis(string $synopsis): self
     {
         $this->synopsis = $synopsis;
+        return $this;
     }
     
-    public function setCreated(string $created_at)
+    public function setCreated(string $created_at): self
     {
         $this->created_at = $created_at;
+        return $this;
     }
 
-    public function setDeleted(string $deleted_at)
+    public function setDeleted(string $deleted_at): self
     {
         $this->deleted_at = $deleted_at;
+        return $this;
     }
 
-    public function setUpdated(string $updated_at)
+    public function setUpdated(string $updated_at): self
     {
         $this->updated_at = $updated_at;
+        return $this;
     }
 }
